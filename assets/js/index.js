0fc9b69254efee60697dec6d477039746a8c4905
trantuananh16032002 <trantuananh16032002@gmail.com>
2024-10-14 22:29:02 +0700

    user

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,12 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
             shopItems.style.display = 'block';
             icon.classList.remove('fa-plus');
             icon.classList.add('fa-xmark');
-            icon.classList.add('rotate'); // Thêm lớp xoay
-        } else {
+            icon.classList.add('rotate'); 
             shopItems.style.display = 'none';
             icon.classList.remove('fa-xmark');
             icon.classList.add('fa-plus');
-            icon.classList.remove('rotate'); // Xóa lớp xoay
+            icon.classList.remove('rotate');
         }
     });
 });
@@ -35,17 +34,22 @@ const loginOption = document.getElementById('loginOption');
 const registerOption = document.getElementById('registerOption');
 const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
-
+const underline = document.getElementById('underline');
 
 loginOption.addEventListener('click', function() {
-  loginForm.style.display = 'block';   
-  registerForm.style.display = 'none'; 
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+    loginOption.classList.add('active');
+    registerOption.classList.remove('active');
+    underline.style.left = '0';
 });
 
-
 registerOption.addEventListener('click', function() {
-  registerForm.style.display = 'block';   
-  loginForm.style.display = 'none';       
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+    registerOption.classList.add('active');
+    loginOption.classList.remove('active');
+    underline.style.left = '50%';
 });
 
 
